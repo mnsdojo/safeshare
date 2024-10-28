bd7ab88b6ebda530ba7b3bae4ab6b35040e98998
sensei <manishkumar97430@gmail.com>
2024-10-28 07:06:50 +0530

    updated readme

---
+++ src/components/file/FileDownload.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FileDownload() {
+  return <div>FileDownload</div>;
+}
+
+export default FileDownload;
---
+++ src/components/file/FileUpload.tsx
@@ -0,0 +1,12 @@
+
+
+
+import React from 'react'
+
+function FileUpload() {
+  return (
+    <div>FileUpload</div>
+  )
+}
+
+export default FileUpload
